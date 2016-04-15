11f6f6f0fdbe829a812ccd7440e4a5247461ff90
Rania Zeineldin <rania@lamppostlabs.com>
2016-04-14 23:01:11 -0400

    serverside validation

--- public/js/script.js
+++ public/js/script.js
@@ -2,164 +2,165 @@
  * Created by lamppostgroup on 2/15/16.
  */
 
-function validateName() { // ----validate name function
-    console.log("name validation:");
-    var name = document.forms["myForm"]["name"].value;
-    var nameErr = document.getElementById('nameErr');
-    if (!name) { // ----check anything in name field
-        console.log("name has no value");
-        return { // ---- if false returns object
-            field: nameErr,
-            message: "name field must be filled out"
-        };
-    }
-    console.log("name is right");
-    return undefined; // ----return if name has value
-}
-
-function validateEmail() { // -----validate email function
-    console.log("email validation:");
-    var email = document.forms["myForm"]["email"].value;
-    var emailErr = document.getElementById('errEmail');
-    var atsign = email.indexOf("@");
-    var period = email.lastIndexOf("."); // ----variable assignment
-    if (!email) { // check that email has a value
-
-        console.log("email has no value");
-        return { // ---- if false returns object
-            field: emailErr,
-            message: "email field must be filled out"
-        };
-    }
-    else if (atsign < 1 || period < atsign + 2 || period + 2 >= email.length) {  // ----check if email field is correct
-        console.log("email doesnt have right characters");
-        return { // ---- if false returns object
-            field: emailErr,
-            message: "email is not valid"
-        };
-    }
-    console.log("email is right");
-    return undefined; // ----if email has value and correct characters it is true
-}
-
-function validateMobile() { // -----validate phone number field
-    console.log("mobile validation:");
-    var mobile = document.forms["myForm"]["mobile"].value;
-    var mobileErr = document.getElementById('errMobile');
-    if (!mobile) { // -----check if anything in mobile field
-        console.log("mobile has no value");
-        return { // ---- if false returns object
-            field: mobileErr,
-            message: "mobile field must be filled out"
-        };
-    }
-    else if (mobile.length < 10 || mobile.length > 10) { // -----make sure mobile is 10 digits
-        console.log("mobile not right length");
-        return { // ---- if false returns object
-            field: mobileErr,
-            message: "mobile not right length"
-        };
-    }
-    else if (isNaN(mobile)) { // ----check to make sure only numbers
-        console.log("mobile not right  characters");
-        return { // ---- if false returns object
-            field: mobileErr,
-            message: "mobile is not number"
-        };
-    }
-    console.log("mobile is right");
-    return undefined; // -----if mobile has value and is right
-}
-
-function validateAge() { // -----validate age field
-    console.log("age validation:");
-    var age = document.forms["myForm"]["age"].value;
-    var ageErr = document.getElementById('errAge');
-
-    if (!age) { // ------check if anything in age field
-        console.log("age has no value");
-        return { // ---- if false returns object
-            field: ageErr,
-            message: "age field must be filled out"
-        };
-    }
-    else if (isNaN(age)) { // ------check to make sure age is number
-        console.log("age is not a number");
-        return { // ---- if false returns object
-            field: ageErr,
-            message: "age is not a number"
-        };
-    }
-    else if (age.length > 3) { // -----check to make sure is no bigger than 3 characters
-        console.log("age not correct length");
-        return { // ---- if false returns object
-            field: ageErr,
-            message: "age is not valid, cannot use more than 3 characters"
-        };
-    }
-    console.log("age is right");
-    return undefined;
-}
-
-function validateGender() { // ----- validate gender field
-    console.log("gender validation:");
-    var female = document.getElementById("female");
-    var male = document.getElementById("male");
-    var genderErr = document.getElementById('errGender');
-    if ((female.checked == false) && (male.checked == false)) {
-        console.log("gender validation not true");
-        return { // ---- if false returns object
-            field: genderErr,
-            message: "gender must be checked"
-        };
-    }
-    return undefined;
-}
-
-function validatePassword() { // ----validate password function
-    console.log("password validation:");
-    var password = document.forms["myForm"]["password"].value;
-    var passwordErr = document.getElementById('errPassword');
-    if (!password) { // ----check if   anything in password field
-        console.log('password has no value');
-        return { // ---- if false returns object
-            field: passwordErr,
-            message: "must enter password"
-        };
-    }
-    else if (password.length <= 6 || password.length >= 12) {   // ----check if password field is correct
-        console.log("password is not correct length");
-        return {  // ---- if false returns object
-            field: passwordErr,
-            message: "password is not correct length"
-        };
-    }
-    console.log("password is correct");
-    return undefined; // ----if password has value and is correct length it will be true
-}
-
-function validateRepassword() { // ----validation re-password function
-    console.log("RE-password validation:");
-    var rePassword = document.forms["myForm"]["repassword"].value;
-    var password = document.forms["myForm"]["password"].value;
-    var rePasswordErr = document.getElementById('errRepassword');
-    if (!rePassword) {
-        console.log('REpassword has no value');
-        return { // ---- if false returns object
-            field: rePasswordErr,
-            message: "must re-enter password"
-        };
-    }
-    else if (rePassword !== password) {
-        console.log('REpassword does not match password');
-        return { // ---- if false returns object
-            field: rePasswordErr,
-            message: "passwords do not match"
-        };
-    }
-    console.log("REpassword is correct");
-    return undefined;
-}
+// log in
+// function validateName() { // ----validate name function
+//    console.log("name validation:");
+//    var name = document.forms["myForm"]["name"].value;
+//    var nameErr = document.getElementById('nameErr');
+//    if (!name) { // ----check anything in name field
+//        console.log("name has no value");
+//        return { // ---- if false returns object
+//            //field: nameErr,
+//            message: "name field must be filled out"
+//        };
+//    }
+//    console.log("name is right");
+//    return undefined; // ----return if name has value
+//}
+//
+//function validateEmail() { // -----validate email function
+//    console.log("email validation:");
+//    var email = document.forms["myForm"]["email"].value;
+//    var emailErr = document.getElementById('errEmail');
+//    var atsign = email.indexOf("@");
+//    var period = email.lastIndexOf("."); // ----variable assignment
+//    if (!email) { // check that email has a value
+//
+//        console.log("email has no value");
+//        return { // ---- if false returns object
+//          //  field: emailErr,
+//            message: "email field must be filled out"
+//        };
+//    }
+//    else if (atsign < 1 || period < atsign + 2 || period + 2 >= email.length) {  // ----check if email field is correct
+//        console.log("email doesnt have right characters");
+//        return { // ---- if false returns object
+//           // field: emailErr,
+//            message: "email is not valid"
+//        };
+//    }
+//    console.log("email is right");
+//    return undefined; // ----if email has value and correct characters it is true
+//}
+//
+//function validateMobile() { // -----validate phone number field
+//    console.log("mobile validation:");
+//    var mobile = document.forms["myForm"]["mobile"].value;
+//    var mobileErr = document.getElementById('errMobile');
+//    if (!mobile) { // -----check if anything in mobile field
+//        console.log("mobile has no value");
+//        return { // ---- if false returns object
+//           // field: mobileErr,
+//            message: "mobile field must be filled out"
+//        };
+//    }
+//    else if (mobile.length < 10 || mobile.length > 10) { // -----make sure mobile is 10 digits
+//        console.log("mobile not right length");
+//        return { // ---- if false returns object
+//            //field: mobileErr,
+//            message: "mobile not right length"
+//        };
+//    }
+//    else if (isNaN(mobile)) { // ----check to make sure only numbers
+//        console.log("mobile not right  characters");
+//        return { // ---- if false returns object
+//          //  field: mobileErr,
+//            message: "mobile is not number"
+//        };
+//    }
+//    console.log("mobile is right");
+//    return undefined; // -----if mobile has value and is right
+//}
+//
+//function validateAge() { // -----validate age field
+//    console.log("age validation:");
+//    var age = document.forms["myForm"]["age"].value;
+//    var ageErr = document.getElementById('errAge');
+//
+//    if (!age) { // ------check if anything in age field
+//        console.log("age has no value");
+//        return { // ---- if false returns object
+//           // field: ageErr,
+//            message: "age field must be filled out"
+//        };
+//    }
+//    else if (isNaN(age)) { // ------check to make sure age is number
+//        console.log("age is not a number");
+//        return { // ---- if false returns object
+//           // field: ageErr,
+//            message: "age is not a number"
+//        };
+//    }
+//    else if (age.length > 3) { // -----check to make sure is no bigger than 3 characters
+//        console.log("age not correct length");
+//        return { // ---- if false returns object
+//           // field: ageErr,
+//            message: "age is not valid, cannot use more than 3 characters"
+//        };
+//    }
+//    console.log("age is right");
+//    return undefined;
+//}
+//
+//function validateGender() { // ----- validate gender field
+//    console.log("gender validation:");
+//    var female = document.getElementById("female");
+//    var male = document.getElementById("male");
+//    var genderErr = document.getElementById('errGender');
+//    if ((female.checked == false) && (male.checked == false)) {
+//        console.log("gender validation not true");
+//        return { // ---- if false returns object
+//       //     field: genderErr,
+//            message: "gender must be checked"
+//        };
+//    }
+//    return undefined;
+//}
+//
+//function validatePassword() { // ----validate password function
+//    console.log("password validation:");
+//    var password = document.forms["myForm"]["password"].value;
+//    var passwordErr = document.getElementById('errPassword');
+//    if (!password) { // ----check if   anything in password field
+//        console.log('password has no value');
+//        return { // ---- if false returns object
+//          //  field: passwordErr,
+//            message: "must enter password"
+//        };
+//    }
+//    else if (password.length <= 6 || password.length >= 12) {   // ----check if password field is correct
+//        console.log("password is not correct length");
+//        return {  // ---- if false returns object
+//          //  field: passwordErr,
+//            message: "password is not correct length"
+//        };
+//    }
+//    console.log("password is correct");
+//    return undefined; // ----if password has value and is correct length it will be true
+//}
+//
+//function validateRepassword() { // ----validation re-password function
+//    console.log("RE-password validation:");
+//    var rePassword = document.forms["myForm"]["repassword"].value;
+//    var password = document.forms["myForm"]["password"].value;
+//    var rePasswordErr = document.getElementById('errRepassword');
+//    if (!rePassword) {
+//        console.log('REpassword has no value');
+//        return { // ---- if false returns object
+//       //     field: rePasswordErr,
+//            message: "must re-enter password"
+//        };
+//    }
+//    else if (rePassword !== password) {
+//        console.log('REpassword does not match password');
+//        return { // ---- if false returns object
+//           // field: rePasswordErr,
+//            message: "passwords do not match"
+//        };
+//    }
+//    console.log("REpassword is correct");
+//    return undefined;
+//}
 
 function storeValues() { // ------  STORES VALUES FROM FORM TO STORE
     console.log("the store value function is being called");
@@ -236,61 +237,61 @@ function logOut() {
     });
 }
 
-function signUpController() { // ----- validation FOR SIGN-UP PAGE
-    var errors = [];
-    console.log("main validation function:");
-    var nameRes = validateName(); // ---- variable for name function
-    var emailRes = validateEmail(); // ----- variable for email function
-    var mobileRes = validateMobile(); // ---- variable for mobile function
-    var ageRes = validateAge(); // ---- variable for age function
-    var genderRes = validateGender();
-    var passRes = validatePassword(); // ----- variable for password function
-    var rePassRes = validateRepassword(); // ----- variable for RE-password function
-
-    if (nameRes) {
-        errors.push(nameRes);
-        console.log("error is found")
-    }
-    if (emailRes) {
-        errors.push(emailRes);
-        console.log("error is found")
-    }
-    if (mobileRes) {
-        errors.push(mobileRes);
-        console.log("error is found")
-    }
-    if (ageRes) {
-        errors.push(ageRes);
-        console.log("error is found")
-    }
-    if (genderRes) {
-        errors.push(genderRes);
-        console.log("error is found")
-    }
-    if (passRes) {
-        errors.push(passRes);
-        console.log("error is found")
-    }
-    if (rePassRes) {
-        errors.push(rePassRes);
-        console.log("error is found")
-    }
-    console.log(JSON.stringify(errors));
-
-    for (var i = 0; i < errors.length; i++) { // ----- loop through array of errors
-        console.log('looping through errors');
-        errors[i].field.innerHTML = errors[i].message;
-    }
-    if (errors.length == 0) { // ---- if no errors, sign up and make cookie/ls
-        console.log('no errors are found store values is called');
-        storeValues();
-        //    return true
-    }
-    console.log(errors.length);
-    console.log('return false');
-    return false;
-
-}
+//function signUpController() { // ----- validation FOR SIGN-UP PAGE
+//    var errors = [];
+//    console.log("main validation function:");
+//    var nameRes = validateName(); // ---- variable for name function
+//    var emailRes = validateEmail(); // ----- variable for email function
+//    var mobileRes = validateMobile(); // ---- variable for mobile function
+//    var ageRes = validateAge(); // ---- variable for age function
+//    var genderRes = validateGender();
+//    var passRes = validatePassword(); // ----- variable for password function
+//    var rePassRes = validateRepassword(); // ----- variable for RE-password function
+//
+//    if (nameRes) {
+//        errors.push(nameRes);
+//        console.log("error is found")
+//    }
+//    if (emailRes) {
+//        errors.push(emailRes);
+//        console.log("error is found")
+//    }
+//    if (mobileRes) {
+//        errors.push(mobileRes);
+//        console.log("error is found")
+//    }
+//    if (ageRes) {
+//        errors.push(ageRes);
+//        console.log("error is found")
+//    }
+//    if (genderRes) {
+//        errors.push(genderRes);
+//        console.log("error is found")
+//    }
+//    if (passRes) {
+//        errors.push(passRes);
+//        console.log("error is found")
+//    }
+//    if (rePassRes) {
+//        errors.push(rePassRes);
+//        console.log("error is found")
+//    }
+//    console.log(JSON.stringify(errors));
+//
+//    for (var i = 0; i < errors.length; i++) { // ----- loop through array of errors
+//        console.log('looping through errors');
+//        errors[i].field.innerHTML = errors[i].message;
+//    }
+//    if (errors.length == 0) { // ---- if no errors, sign up and make cookie/ls
+//        console.log('no errors are found store values is called');
+//        storeValues();
+//        //    return true
+//    }
+//    console.log(errors.length);
+//    console.log('return false');
+//    return false;
+//
+//}
 
 function logInController() { // ----validation FOR LOGIN
     var errors = [];
--- server.js
+++ server.js
@@ -13,6 +13,8 @@ var cookieParser = require('cookie-parser');
 db = mongoose.connection;
 mongoose.connect('mongodb://localhost/test');
 
+var signup = require('./public/js/signupValidation.js');
+//console.log(signup.validatePassword('42374'));
 //setTimeout(function () {
 //    var http = require('http');
 //    var querystring = require('querystring');
@@ -119,20 +121,53 @@ app.get('/music', function (req, res) {
 
 app.post('/signup', function (req, res) {
     console.log('req body', req.body);
-    //var userString = JSON.stringify(req.body);
-    //// res.send(req.cookies.user);
-    //console.log('right before cookie is made');
-    //res.cookie('user', userString);
-    //console.log('cookies', req.cookies);
-
-    var nameRes = validateName();
-    var emailRes = validateEmail();
-    var mobileRes = validateMobile();
-    var ageRes = validateAge();
-    var genderRes = validateGender();
-    var passRes = validatePassword();
-    var rePassRes = validateRepassword();
-
+    var userString = JSON.stringify(req.body);
+    // res.send(req.cookies.user);
+    console.log('right before cookie is made');
+    res.cookie('user', userString);
+    console.log('cookies', req.cookies);
+    var errors = [];
+    var nameRes = signup.validateName(req.body.name);
+    var emailRes = signup.validateEmail(req.body.email);
+    var mobileRes = signup.validateMobile(req.body.mobile);
+    var ageRes = signup.validateAge(req.body.age);
+    var genderRes = signup.validateGender(req.body.gender);
+    var passRes = signup.validatePassword(req.body.password);
+    var rePassRes = signup.validateRepassword(req.body.repassword, req.body.password);
+    if (nameRes) {
+        errors.push(nameRes);
+        console.log("error is found")
+    }
+    if (emailRes) {
+        errors.push(emailRes);
+        console.log("error is found")
+    }
+    if (mobileRes) {
+        errors.push(mobileRes);
+        console.log("error is found")
+    }
+    if (ageRes) {
+        errors.push(ageRes);
+        console.log("error is found")
+    }
+    if (genderRes) {
+        errors.push(genderRes);
+        console.log("error is found")
+    }
+    if (passRes) {
+        errors.push(passRes);
+        console.log("error is found")
+    }
+    if (rePassRes) {
+        errors.push(rePassRes);
+        console.log("error is found")
+    }
+    if (errors != 0) {
+        return res.render('./views/signup', {
+            errors: errors,
+            body: req.body
+        });
+    }
     User.findOne({email: req.body.email}, function (err, user) {
         if (user) {
             console.log('existing user');
@@ -153,14 +188,31 @@ app.post('/signup', function (req, res) {
 });
 
 app.post('/login', function (req, res) {
+    console.log(req.body);
+    var errors = [];
     var query = {
         email: req.body.email,
         password: req.body.password
     };
-    User.find(query, function (err, user) {
-        res.send(user);
+    console.log("main validation function:");
+    var emailRes = signup.validateEmail(req.body.email); // -----setting variable for email
+    var passRes = signup.validatePassword(req.body.password); // ----setting variable for password
+    if (emailRes) {
+        errors.push(emailRes);
+        console.log("error is found")
+    }
+    if (passRes) {
+        errors.push(passRes);
+        console.log("error is found")
+    }
+    if (errors != 0) {
+        return res.render('./views/login', {
+            errors: errors,
+            body: query
+        });
+    }
+    User.find( query, function (err, user) {
         console.log('user', user);
-
         var sessUser = new SessionUser();
         sessUser.user = user[0]._id;
         sessUser.save(function (err, user) {
@@ -168,6 +220,8 @@ app.post('/login', function (req, res) {
                 return console.log(err);
             }
             console.log('new', user);
+            console.log('before redirect');
+            return res.redirect('/index');
         })
     })
 });
